43be3c84fc54dac4300e779e4a15470291800c71
Joe Haddad <92923559+joehaddad1000@users.noreply.github.com>
2022-05-06 13:25:59 +0300

    new program to see PI knowledge
    fun test #1

---
+++ main1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+int main()
+{
+      printf("Can you tell me what PI value is?\n");
+      const float PI = 3.14;
+      float answer1;
+      scanf("%f",&answer1);
+            while (answer1!=PI)
+            {
+                  if (answer1==PI)
+                  {
+                        printf("PI is indeed %f\n",answer1);
+                  }
+                        else if(answer1!=PI)
+                        {
+                              printf("%.2f \? Really\?\nwhat the FALAFEL son, you don't know the value of PI???\nPI is %.2f\n",answer1,PI);
+                              printf("Let's retry it!\n");
+                        }
+                  printf("Can you tell me what PI value is?\n");      
+                  scanf("%f",&answer1);
+            }
+         printf("thanks my son\n");   
+     return 0;                   
+}
