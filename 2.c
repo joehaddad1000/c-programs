9075334513b6f981e2c50088de910672d4e0cb5c
Joe Haddad <92923559+joehaddad1000@users.noreply.github.com>
2022-06-08 15:45:21 +0300

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float s = 0, n, x;
+    int i, c = 1;
+    do
+    {
+        scanf("%f%f", &n, &x);
+    } while (n >= 30);
+    {
+        for (i; i <= 30; n++)
+        {
+            s = c * ((n * x) / (n + x))
+                        c = -1;
+        }
+    }
+}
